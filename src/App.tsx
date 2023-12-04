2024ef425b2f39bd06040fb52e520a2b38f608a3
enzgamilla <laurenceangelogamilla@gmail.com>
2023-12-04 20:35:20 +0400

    add grid layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+import { breakpoints } from "./style/StyleChakraUI";
+
 function App() {
-  return <></>;
+  return (
+    <Grid templateAreas={breakpoints}>
+      <GridItem pl={"2"} bg={"orange.300"} area={"header"}>
+        Header
+      </GridItem>
+      <Show above="lg">
+        <GridItem pl={"2"} bg={"pink.300"} area={"side-nav"}>
+          Side Navigation Bar
+        </GridItem>
+      </Show>
+      <GridItem pl={"2"} bg={"green.300"} area={"main"}>
+        Main Body
+      </GridItem>
+      <GridItem pl={"2"} bg={"blue.300"} area={"footer"}>
+        Footer
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
---
+++ src/style/StyleChakraUI.ts
@@ -0,0 +1,4 @@
+export const breakpoints = {
+  base: `"header" "main" "footer"`,
+  lg: `"header header" "side-nav main" "side-nav footer"`,
+};
