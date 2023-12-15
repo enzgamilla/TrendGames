1e85df29efb7fd9b8de9a66d2105f249ca192802
enzgamilla <laurenceangelogamilla@gmail.com>
2023-12-15 20:08:41 +0400

    add null if error occurs

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,4 +1,4 @@
-import { SimpleGrid, Text } from "@chakra-ui/react";
+import { SimpleGrid } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 import { GameContrainerGrid } from "../style-chakraUI/StyleChakraUI";
@@ -9,9 +9,10 @@ const GameGrid = () => {
   const { error, data, isLoading } = useGames();
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
+  if (error) return null;
+
   return (
     <>
-      {error && <Text>{error}</Text>}
       <SimpleGrid columns={GameContrainerGrid} padding="10px" spacing={3}>
         {isLoading &&
           skeletons.map((skeleton) => (
