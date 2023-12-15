2cc3dbb34f3aa3646be0a5dfbb8e0f23e29c1435
enzgamilla <laurenceangelogamilla@gmail.com>
2023-12-15 15:07:25 +0400

    build icon platform list each games

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,10 +2,17 @@ import { useState, useEffect } from "react";
 import apiClient from "../services/apiClient";
 import { CanceledError } from "axios";
 
+export interface Platform {
+  id: number;
+  slug: string;
+  name: string;
+}
+
 export interface Game {
   id: number;
   name: string;
   background_image: string;
+  parent_platforms: { platform: Platform }[];
 }
 
 interface FetchGames {
@@ -16,6 +23,8 @@ interface FetchGames {
 const useGames = () => {
   const [error, setError] = useState("");
   const [games, setGames] = useState<Game[]>([]);
+
+  console.log(games);
 
   useEffect(() => {
     const controller = new AbortController();
